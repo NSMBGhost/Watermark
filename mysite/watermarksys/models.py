4d024865bebc8ce0915c56e636f5357b2413b6b6
parker <huangzhenbo741@163.com>
2021-04-15 13:19:50 +0800

    0415v1

--- mysite/watermarksys/models.py
+++ mysite/watermarksys/models.py
@@ -8,10 +8,12 @@ class userinformation(models.Model):
     address=models.CharField(max_length=40)
     money=models.DecimalField(default=0,max_digits=12,decimal_places=2)
 class watermark(models.Model):
-    phone=models.IntegerField(unique=True)
+    phone=models.IntegerField()
     upload_time=models.CharField(max_length=20)
+    syspath=models.CharField(max_length=30,default="")
+    filename=models.CharField(max_length=15,default="")
 class cost(models.Model):
-    phone = models.IntegerField(unique=True)
+    phone = models.IntegerField()
     cost_time=models.CharField(max_length=40)
     cost_value=models.DecimalField(max_digits=12,decimal_places=2)
     cost_state=models.IntegerField()
--- mysite/watermarksys/views.py
+++ mysite/watermarksys/views.py
@@ -6,4 +6,18 @@ def index(request):
     list=users.objects.all()
     context = {'latest_question_list': list}
     return render(request, 'watermarksys/index.html', context)
+def login(request):
+    try:
+        getuser=users.objects.get(phone=request.POST['phone'])
+        if(getuser.password==request.POST['password']):
+            return render(request,'watermarksys/main.html')
+    except:
+        return render(request,'watermarksys/index.html')
+def getregister(request):
+    watermarks=watermark.objects.all()
+
+    context = {'imagepath': watermarks}
+    return render(request,'watermarksys/register.html',context)
+
+
 # Create your views here.
